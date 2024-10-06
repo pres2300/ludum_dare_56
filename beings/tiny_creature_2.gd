320b602635fa4b4f99df2f3a0d8b035d7550b3fd
Jacob Preston <pres2300@gmail.com>
2024-10-06 16:54:40 -0700

    new enemy files

---
+++ beings/tiny_creature_2.gd
@@ -0,0 +1,14 @@
+extends "res://beings/enemy_base.gd"
+
+var move_speed: int = 500
+var direction: Vector2 = Vector2(0,0)
+
+func _ready() -> void:
+	var target_point: Vector2 = get_tree().get_first_node_in_group("Player").global_position
+	direction = (target_point-global_position).normalized()
+
+func _process(delta: float) -> void:
+	global_position += direction*move_speed*delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
